404fd83a6936550c1f6d94c0a7eab03ad520f01f
Dave Kimber <dkimber@greshamtech.com>
2018-02-03 19:13:56 +0000

    more clojure

---
+++ Clojure/fwpd/src/fwpd/core.clj
@@ -0,0 +1,40 @@
+(ns fwpd.core)
+(def filename "suspects.csv")
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [file-contents]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split file-contents #"\n")))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (map :name (filter #(>= (:glitter-index %) minimum-glitter) records)))
+
+(defn append
+  [name glitter-index]
+  (spit filename (str name "," glitter-index "\n") :append true))
+
